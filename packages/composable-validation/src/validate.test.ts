5b7f23b49c38ef7033d6217e086133ccc71cd28e
Matt Hawes <Matt.Hawes@softwire.com>
2017-09-01 17:09:47 +0100

    Changes to class based structure, support polymorphisim

--- packages/composable-validation/src/validate.test.ts
+++ packages/composable-validation/src/validate.test.ts
@@ -1,11 +1,16 @@
 import { endsWith, flatten, startsWith } from 'lodash';
-import { hasValidationErrors, rules, validate, ValueValidator, Validator } from './validate';
+import { hasValidationErrors, rules, ObjectValidator, ValueValidator, Validator } from './validate';
+import {} from 'jest';
 
-export const maxLength = (max: number): ValueValidator<string | null> =>
-  (value: string | null) => (value != null && value.length > max) ? [`Text must be less than ${max} characters`] : [];
+export const maxLength = (max: number): Validator<string | null> => ({
+  validate: (value: string | null) =>
+    (value != null && value.length > max) ? [`Text must be less than ${max} characters`] : []
+});
 
-export const minLength = (min: number): ValueValidator<string> =>
-  (value: string) => value.length < min ? [`Must be at least ${min} characters`] : [];
+export const minLength = (min: number): Validator<string | null> => ({
+  validate: (value: string | null) =>
+    (value != null && value.length < min) ? [`Must be at least ${min} characters`] : []
+});
 
 describe('rules', () => {
   it('will return the result of all of the individual validators, combined into a single array', () => {
@@ -32,7 +37,7 @@ describe('rules', () => {
 describe('validate', () => {
   it('returns an object with key value pairs for values which fail validation', () => {
     type MyObject = { name: string };
-    const constraints: Validator<MyObject> = { name: rules(minLength(5), maxLength(10)) };
+    const constraints: Validator<MyObject> = new ObjectValidator({ name: rules(minLength(5), maxLength(10)) });
     const validationResult = (validate(constraints, { name: '' }) as any).name;
 
     expect(validationResult && validationResult.length).toBeGreaterThan(0);
--- packages/composable-validation/src/validate.ts
+++ packages/composable-validation/src/validate.ts
@@ -2,33 +2,44 @@ import { chain, flatMap, mapValues, pickBy, some } from 'lodash';
 
 export type ValidationErrors = Array<string>;
 export type ValueValidator<T> = (value: T) => ValidationErrors;
-
-export type Validator<T> = {
-  [P in keyof T]?: ValueValidator<T[P]>
+export type Template<T> = {
+  [P in keyof T]?: Validator<T[P]>
 };
 
+export interface Validator<T> {
+  validate(item: T): ValidationErrors | ValidationResult<T>
+}
+
+export class ObjectValidator<T> implements Validator<T> {
+  private template: Template<T>;
+
+  constructor(temp: Template<T>) { this.template = temp }
+
+  validate(object: T) {
+    return pickBy(
+      mapValues(
+        this.template,
+        <K extends keyof T>(validator: Validator<T[K]>, key: K) => validator.validate(object[key]),
+      ),
+      (errors) => errors && errors.length > 0,
+    )
+  }
+}
+
 export type ValidationResult<T> = {
-  [P in keyof T]?: ValidationErrors
+  [P in keyof T]?: ValidationErrors | ValidationResult<T[P]>
 };
 
 export const rules = <T>(...validators: Array<ValueValidator<T>>): ValueValidator<T> =>
   (value) => flatMap(validators, (validator) => validator(value));
 
+// ToDo fix validateKey
 export const validateKey =
   <T, K extends keyof T>(objectValidator: Validator<T>, key: K, value: T[K]): ValidationErrors => {
     const validator: ValueValidator<T[K]> | undefined = objectValidator[key];
     return validator != null ? validator(value) : [];
   };
 
-export const validate = <T>(objectValidator: Validator<T>, object: T): ValidationResult<T> =>
-  pickBy(
-    mapValues(
-      objectValidator,
-      <K extends keyof T>(validateValue: ValueValidator<T[K]>, key: K) =>
-        validateValue(object[key]),
-    ) as ValidationResult<T>,
-    (errors) => errors && errors.length > 0,
-  );
 
 export const hasValidationErrors = <T>(result: ValidationResult<T>): boolean =>
   some(result as object, (validationErrors: ValidationErrors) => (validationErrors && validationErrors.length > 0));
